89e10f708ea10a46348a6e417e5e4831253796c0
Guilherme Viana <ggviana@hotmail.com.br>
2020-04-16 15:35:47 -0300

    Added new build scripts

--- src/app.js
+++ src/app.js
@@ -1,44 +1,39 @@
-;(function (window, document) {
-  "use strict"
+(function (window, document) {
+  'use strict'
 
-  const queryAll = (selector) => Array.from(document.querySelectorAll(selector))
+  const $ = selector => Array.from(document.querySelectorAll(selector))
 
-  const sections = queryAll('section')
-    .map(section => ({
-      startPos: Math.round(section.getBoundingClientRect().top + window.scrollY) - 1,
-      fragment: section.id
-    }))
+  const sections = $('section').map(section => ({
+    startPos:
+      Math.round(section.getBoundingClientRect().top + window.scrollY) - 1,
+    fragment: section.id
+  }))
 
-  function getCurrentFragment () {
-    var fragment = sections[0].fragment
+  const getCurrentFragment = () =>
+    sections.reduce(
+      (fragment, section) =>
+        window.scrollY >= section.startPos ? section.fragment : fragment,
+      sections[0].fragment
+    )
 
-    sections.forEach(section => {
-      if (window.scrollY >= section.startPos) {
-        fragment = section.fragment
-      }
-    })
-
-    return fragment
-  }
-
-  ;(function ChangeFragmentOnScroll() {
-
-    function handler (event) {
-      history.pushState(null, null, `#${getCurrentFragment()}`)
-    }
+  ;(function ChangeFragmentOnScroll () {
+    const handler = () =>
+      window.history.pushState(null, null, `#${getCurrentFragment()}`)
 
     window.addEventListener('scroll', handler)
     handler()
-  }())
+  })()
 
   ;(function AnimateProjectList () {
-
     const showingContainer = document.querySelector('.project-list')
-    const pixelsBefore = 400
-    const limit = showingContainer.getBoundingClientRect().top - pixelsBefore
+    const pixelsAround = 200
+    const rectangle = showingContainer.getBoundingClientRect()
+    const isWithinLimits = height =>
+      rectangle.top - pixelsAround >= height ||
+      rectangle.bottom + pixelsAround <= height
 
-    function handler () {
-      if (window.scrollY >= limit) {
+    const handler = () => {
+      if (isWithinLimits(window.scrollY)) {
         showingContainer.classList.toggle('showing')
         window.removeEventListener('scroll', handler)
       }
@@ -46,38 +41,5 @@
 
     window.addEventListener('scroll', handler)
     handler()
-  }())
-
-  ;(function SmoothScroll () {
-
-    //const body = /firefox|trident/i.test(navigator.userAgent) ? document.documentElement : document.body
-    const body = document.body
-    const DURATION = 900
-
-    queryAll('.move-to')
-      .map(link => 
-        link.addEventListener('click', event => {
-          var startTime
-          var startPos = body.scrollTop
-          var fragment = /[^#]+$/.exec(event.target.href)[0]
-          var endPos = document.getElementById(fragment).getBoundingClientRect().top
-          var maxScroll = body.scrollHeight - window.innerHeight
-          var scrollEndValue = startPos + endPos < maxScroll ? endPos : maxScroll - startPos
-          function scroll (timestamp) {
-            startTime = startTime || timestamp
-            var elapsed = timestamp - startTime
-            var progress = easeInOutCubic(elapsed, startPos, scrollEndValue, DURATION)
-            body.scrollTop = progress
-            elapsed < DURATION && window.requestAnimationFrame(scroll)
-          }
-          window.requestAnimationFrame(scroll)
-          event.preventDefault()
-        })
-      )
-
-    function easeInOutCubic (t, b, c, d) {
-      if ((t/=d/2) < 1) return c/2*t*t*t + b
-      return c/2*((t-=2)*t*t + 2) + b
-    }
-  }())
-}(window, document));
+  })()
+})(window, document)
